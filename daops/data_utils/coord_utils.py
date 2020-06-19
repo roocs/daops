81c0f3e7abbdaf73ab7b060abd893008f9d1e10d
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-06-19 13:59:38 +0100

    missing scalar coord fix and test

--- daops/data_utils/coord_utils.py
+++ daops/data_utils/coord_utils.py
@@ -1,12 +1,30 @@
-
+import numpy as np
 
 def squeeze_dims(da, *args, **kwargs):
     """
-
-    :param ds: Xarray DataArray
+    :param da: Xarray DataArray
     :param args: sequence of arguments
     :param kwargs: dictionary of arguments
     :return: Xarray DataArray
     """
     da_squeezed = da.squeeze(*args, **kwargs)
     return da_squeezed
+
+
+def add_scalar_coord(ds, *args, **kwargs):
+    """
+    :param da: Xarray DataArray
+    :param args: sequence of arguments
+    :param kwargs: dictionary of arguments
+    :return: Xarray DataArray
+    """
+    coord = kwargs.get('id')
+    value = kwargs.get('value')
+    dtype = kwargs.get('dtype')
+
+    ds = ds.assign_coords({f'{coord}': np.array(value, dtype=dtype)})
+
+    for k, v in kwargs.get('attrs').items():
+        ds[coord].attrs[k] = v
+
+    return ds
