9324deacac71f6ae62cc098512a9cece15827d0b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-06-22 16:47:23 +0200

    Bump version: 0.1.0 → 0.2.0

--- daops/__init__.py
+++ daops/__init__.py
@@ -7,7 +7,7 @@ __author__ = """Elle Smith"""
 __contact__ = "eleanor.smith@stfc.ac.uk"
 __copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
 __license__ = "BSD"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 # from .utils import *
 # from .ops import (
