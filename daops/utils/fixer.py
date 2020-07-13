6f138a717c5521274babe116f60d8640f6f77cb9
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-07-13 09:52:04 +0100

    tidying up fixer

--- daops/utils/fixer.py
+++ daops/utils/fixer.py
@@ -19,46 +19,6 @@ class FuncChainer(object):
         return result
 
 
-# class Fixer(object):
-#
-#     FIX_DIR = './fixes'
-#
-#     def __init__(self, ds_id):
-#         self.ds_id = ds_id
-#         self._lookup_fix()
-#
-#     def _lookup_fix(self):
-#         fix_file = os.path.join(self.FIX_DIR, f'{self.ds_id}.json')
-#
-#         if not os.path.isfile(fix_file):
-#             self.pre_processor = None
-#             self.post_processor = None
-#             self.pre_processors = ()
-#
-#         else:
-#             content = json.load(open(fix_file))
-#             pre_processors = content.get('pre_processors', None)
-#             post_processors = content.get('post_processors', None)
-#
-#             if pre_processors:
-#                 self.pre_processors = []
-#                 for pre_processor in pre_processors:
-#                     self.pre_processors.append(resolve_import(pre_processor['func']))
-#             else:
-#                 self.pre_processors = ()
-#             self.pre_processor = FuncChainer(self.pre_processors)
-#
-#             if post_processors:
-#                 post_process_list = []
-#                 for post_processor in post_processors:
-#                     post_process_list.append((resolve_import(post_processor['func']),
-#                                               post_processor.get('args', None) or [],
-#                                               post_processor.get('kwargs', None) or {}))
-#                 self.post_processor = post_process_list
-#             else:
-#                 self.post_processor = ()
-
-
 class Fixer(object):
 
     def __init__(self, ds_id):
