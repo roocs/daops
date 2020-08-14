61f2e6ae4e899ed3f889f68145e8607256358e7f
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-08-14 10:17:00 +0100

    updated roocs-utils install

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     # This qualifier can be used to selectively exclude Python versions -
     # in this case early Python 2 and 3 releases
     python_requires='>=3.6.0',
-    install_requires=[requirements, 'roocs_utils @ git+https://github.com/roocs/roocs_utils.git'],
+    install_requires=[requirements, 'roocs_utils @ git+https://github.com/roocs/roocs-utils.git'],
     long_description=_long_description,
     long_description_content_type='text/markdown',
     include_package_data=True,
