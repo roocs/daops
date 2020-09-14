fcde776004cce405b4fc803994f90f2683d2d0d8
Eleanor Smith <esmith88@jasmin-sci2.ceda.ac.uk>
2020-09-14 15:35:12 +0100

    fix for logging

--- daops/__init__.py
+++ daops/__init__.py
@@ -10,7 +10,11 @@ __license__ = "BSD"
 __version__ = "0.2.0"
 
 from roocs_utils.config import get_config
-from clisops import logging
-import daops
 
+import daops
 CONFIG = get_config(daops)
+
+from clisops import logging
+
+
+
