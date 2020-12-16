3f72a7d5f70dd4840929f582b0bfadfc1b3ea035
Eleanor Smith <esmith88@sci5.jasmin.ac.uk>
2020-12-16 12:26:10 +0000

    update notebook to work with fix

--- tests/test_utils/test_normalise.py
+++ tests/test_utils/test_normalise.py
@@ -37,9 +37,9 @@ def test_file_uris_files():
             (
                 "CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r1i1p1f1.Amon.rlds.gr.v20180803",
                 [
-                    "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Amon"
-                    "/rlds/gr/v20180803/rlds_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_"
-                    "gr_185001-201412.nc"
+                    "tests/mini-esgf-data/test_data/badc/cmip6/data/CMIP6/CMIP/IPSL"
+                    "/IPSL-CM6A-LR/historical/r1i1p1f1/Amon/rlds/gr/v20180803"
+                    "/rlds_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_185001-201412.nc"
                 ],
             )
         ]
@@ -48,7 +48,6 @@ def test_file_uris_files():
     for ds_id, file in file_path.items():
         result.add(ds_id, file)
     assert result.file_uris == [
-        "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Amon"
-        "/rlds/gr/v20180803/rlds_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_"
-        "gr_185001-201412.nc"
+        "tests/mini-esgf-data/test_data/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical"
+        "/r1i1p1f1/Amon/rlds/gr/v20180803/rlds_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_185001-201412.nc"
     ]
