c79e8a2704e1e70ae7ed2673a9e514a6a5e643b3
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-04-12 14:36:47 +0200

    use utils.common for logging functions

--- daops/__init__.py
+++ daops/__init__.py
@@ -8,8 +8,16 @@ __copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
 __license__ = "BSD"
 __version__ = "0.7.0"
 
+from loguru import logger
+
 from roocs_utils.config import get_config
 
 import daops
 
 CONFIG = get_config(daops)
+
+from .utils.common import enable_logging  # noqa
+
+# Disable logging for daops and remove the logger that is instantiated on import
+logger.disable("daops")
+logger.remove()
