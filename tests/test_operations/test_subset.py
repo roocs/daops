2f280056af77116f815afca9672058fa67220cf2
Alan Iwi <alan.iwi@stfc.ac.uk>
2023-03-05 18:17:52 +0000

    similar to previous commit, but for test_subset.py

--- tests/test_operations/test_subset.py
+++ tests/test_operations/test_subset.py
@@ -215,10 +215,10 @@ def test_subset_t_with_invalid_date(tmpdir, load_esgf_test_data):
             output_dir=tmpdir,
             file_namer="simple",
         )
-        assert (
-            exc.value == "No files found in given time range for "
-            "cmip5.output1.MOHC.HadGEM2-ES.rcp85.mon.atmos.Amon.r1i1p1.latest.tas"
-        )
+    assert (
+        str(exc.value) == "No files found in given time range for "
+        "cmip5.output1.MOHC.HadGEM2-ES.rcp85.mon.atmos.Amon.r1i1p1.latest.tas"
+    )
 
 
 @pytest.fixture(
