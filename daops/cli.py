ad9004a57fa41add97386aa904ebb3ba55821cdd
Alan Iwi <alan.iwi@stfc.ac.uk>
2023-03-05 13:09:20 +0000

    in CLI: print file URLs; fix missing "levels" param; strip out code where stuff from roocs_utils can be relied on instead

--- daops/cli.py
+++ daops/cli.py
@@ -5,55 +5,31 @@ __contact__ = 'alan.iwi@stfc.ac.uk'
 __copyright__ = "Copyright 2023 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level package directory"
 
-
 import argparse
 import sys
 import dateutil.parser
 
 from daops.ops.subset import subset
-
-
-def area_arg(val):
-    bits = val.split(',')
-    if len(bits) != 4:
-        raise ValueError
-    return [float(bit) for bit in bits]
+from roocs_utils.parameter.param_utils import time_interval
+from roocs_utils.parameter.param_utils import time_components
+from roocs_utils.parameter.param_utils import level_series
 
 
 def time_window_arg(val):
-    bits = val.split(',')
-    if len(bits) != 2:
-        raise ValueError
-    return [dateutil.parser.parse(bit) for bit in bits]
-
-
-def time_component(val):
-    time_type, time_vals_str = val.split(':')
-    bits = time_vals_str.split(',')
-    if time_type in ('year', 'day', 'hour', 'minute', 'second'):
-        time_vals = [int(bit) for bit in bits]
-    elif time_type == 'month':
-        time_vals = []
-        for bit in bits:
-            try:
-                time_vals.append(int(bit))
-            except ValueError:
-                time_vals.append(bit)
-    else:
-        raise ValueError
-    return time_type, time_vals
+    start, end = val.split('/')
+    return time_interval(start, end)
 
 
 def time_components_arg(val):
     d = {}
     for bit in val.split('|'):
-        k, v = time_component(bit)
-        d[k] = v
-    return d
+        time_type, time_vals_str = val.split(':')
+        d[time_type] = time_vals_str
+    return time_components(**d)
 
 
 def levels_arg(val):
-    return [float(bit) for bit in val.split(',')]
+    return level_series(val)
 
 
 def parse_args():
@@ -63,9 +39,11 @@ def parse_args():
     sub_parsers = parser.add_subparsers()
 
     parser_subset = sub_parsers.add_parser('subset', help='subset data')
-    parser_subset.add_argument('--area', '-a', type=area_arg, metavar='w,s,e,n')
+    parser_subset.add_argument('--area', '-a', type=str, metavar='area',
+                               help=('area in format w,s,e,n. Hint: if w is negative, include an "=" sign '
+                                     'e.g. --area=-10,...'))
     parser_subset.add_argument('--time', '-t', type=time_window_arg, metavar='time_window',
-                               help='time window e.g. 1999-01-01T00:00:00,2100-12-30T00:00:00')
+                               help='time window e.g. 1999-01-01T00:00:00/2100-12-30T00:00:00')
     parser_subset.add_argument('--time-components', '-c', type=time_components_arg, metavar='time_components',
                                help="time components e.g. month:dec,jan,feb or 'year:1970,1980|month:01,02,03'")
     parser_subset.add_argument('--levels', '-l', type=levels_arg,
@@ -83,6 +61,7 @@ def get_params(args):
             'time': args.time,
             'time_components': args.time_components,
             'area': args.area,
+            'level': args.levels,
             'output_type': args.output_format,
             'output_dir': args.output_dir,
             'collection': args.collection,
@@ -93,9 +72,9 @@ def get_params(args):
 def main():
     args = parse_args()
     params = get_params(args)
-    print(params)
     ret = subset(**params)
-    print(ret.file_uris)
+    for uri in ret.file_uris:
+        print(uri)
 
 
 if __name__ == "__main__":
