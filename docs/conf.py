a9a8e25b0484b4c9f4f8579395c5a5833dbaa5f0
MacPingu <cehbrecht@users.noreply.github.com>
2023-11-17 14:48:05 +0100

    Bump version: 0.8.1 → 0.9.0
    * update reqs
    
    * update history
    
    * updete dev requirements
    
    * update dependency to clisops >=0.10
    
    * test also on python 3.10 and 3.11
    
    * enabled lint
    
    * pep8
    
    * update rtd config
    
    * fix errors in docs build
    
    * Update .pre-commit-config.yaml
    
    Co-authored-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
    
    * Update .pre-commit-config.yaml
    
    Co-authored-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
    
    * Update .pre-commit-config.yaml
    
    Co-authored-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
    
    * Update HISTORY.rst
    
    Co-authored-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
    
    * update tox version
    
    * Bump version: 0.8.1 → 0.9.0
    
    ---------
    
    Co-authored-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ author = "Elle Smith"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.8.1"
+version = "0.9.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -83,7 +83,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
